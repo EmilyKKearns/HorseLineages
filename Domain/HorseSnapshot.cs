524f12718851fa47ae82877ad49485cf8ad726fe
Emily Kearns <emilykkearns@gmail.com>
2025-11-03 17:04:46 -0700

    Scaffolding plus added snapshot to domain and migration

---
+++ Domain/HorseSnapshot.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Domain;
+
+public class HorseSnapshot
+{
+    public int HorseSnapshotId { get; set; }
+    public required int HorseId { get; set; }
+    public int Parent1HorseId { get; set; }
+    public int Parent2HorseId { get; set; }
+    public required string Rank { get; set; }
+    public required int Score { get; set; }
+    public required int StarLevel { get; set; }
+    public required int PotentialLevel { get; set; }
+    public required int UniqueSkillLevel { get; set; }
+    public required int Speed { get; set; }
+    public required int Stamina { get; set; }
+    public required int Power { get; set; }
+    public required int Guts { get; set; }
+    public required int Wit { get; set; }
+    public required bool IsGuest { get; set; }
+    public string? GuestOwnerName { get; set; }
+    public int GuestParent1HorseId { get; set; }
+    public int GuestParent2HorseId { get; set; }
+}
--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff0360437ad3b50c4a052473cd3dbf7a16ffd01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
